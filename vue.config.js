9b73f12de492be581e691e15289441f915b4fd70
陈敬睿 <fz_marchen@163.com>
2025-10-21 20:03:06 +0800

    feat: 写死路径

--- vue.config.js
+++ vue.config.js
@@ -1,7 +1,8 @@
 const { defineConfig } = require('@vue/cli-service')
 module.exports = defineConfig({
-  publicPath: process.env.NODE_ENV === 'production'
-    ? '/vue-json-schema-form-demo/' // 替换为你的 GitHub 仓库名
-    : '/',
+  // publicPath: process.env.NODE_ENV === 'production'
+  //   ? '/vue-json-schema-form-demo/' // 替换为你的 GitHub 仓库名
+  //   : '/',
+  publicPath: '/vue-json-schema-form-demo/',
   transpileDependencies: true
 })
