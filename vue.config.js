53f7cf26f24a94ac93414889f2032f55692eb49b
陈敬睿 <fz_marchen@163.com>
2025-10-21 19:56:43 +0800

    feat: 添加二级路径

--- vue.config.js
+++ vue.config.js
@@ -1,4 +1,7 @@
 const { defineConfig } = require('@vue/cli-service')
 module.exports = defineConfig({
+  publicPath: process.env.NODE_ENV === 'production'
+    ? '/vue-json-schema-form-demo/' // 替换为你的 GitHub 仓库名
+    : '/',
   transpileDependencies: true
 })
